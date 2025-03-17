30a583333ca645bec266ce30d7bae3dc6e0c11fc
OlamideLanre <lanreprincess@gmail.com>
2025-03-17 17:35:12 +0100

    eloquent class and migration

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,37 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return  [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Clerk',
-                'salary' => '$30,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Strategist',
-                'salary' => '$40,000'
-            ]
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $Findjob = Arr::first(static::all(), fn($jobs) => $jobs['id'] == $id);
-        if (!$Findjob) {
-            abort(404);
-        }
-        return $Findjob;
-    }
+    protected $table = 'job_listing';
+    protected $fillable = ['title', 'salary']; //represents all attributes that are allowed to be mass assigned
 }

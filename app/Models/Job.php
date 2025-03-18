79202f48a5b51d970043c7f8a48186d7cc2a108c
OlamideLanre <lanreprincess@gmail.com>
2025-03-18 10:17:56 +0100

    Model factories & eloquent relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,11 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 class Job extends Model
 {
+    use HasFactory;
     protected $table = 'job_listing';
     protected $fillable = ['title', 'salary']; //represents all attributes that are allowed to be mass assigned
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
